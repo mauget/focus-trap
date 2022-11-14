d10e13e79b944322304a7e7ae74fe659475fe75c
mauget <mauget@gmail.com>
2022-11-13 20:13:13 -0500

    Refactor trap logic

--- src/handlers/trapFocusInComponent.js
+++ src/handlers/trapFocusInComponent.js
@@ -5,37 +5,43 @@ const trapFocusInComponent = (event, refFocusTrap) => {
     // No tab, no interest
     if (event.key !== 'Tab') return;
 
-    // Core: collect ordered focusable elements with a filtered dom query
+    // Initial housekeeping ...
+
+    // Core data: collect ordered focusable child elements
+    const { current: rootElement } = refFocusTrap;
     const query = 'a[href], button:not([disabled]), textarea, input, select';
-    const focusableElements = refFocusTrap.current.querySelectorAll(query);
+
+    const focusableElements = rootElement.querySelectorAll(query);
+    const { length: elementCount } = focusableElements;
 
     // Maybe there are no focusable elements?
-    if (focusableElements.length === 0) return
+    if (elementCount === 0) return
 
-    // Capture first and last elements of ordered list (both could be same element)
+    // Capture first and last elements of ordered list (each could be same element)
     const firstElement = focusableElements[0]
-    const lastElement = focusableElements[focusableElements.length - 1]
+    const lastElement = focusableElements[elementCount - 1]
 
-    const backwardDirection = event.shiftKey;
+    const { shiftKey: backwardDirection } = event;
     const forwardDirection = !backwardDirection;
 
-    // Wrap focus if tab attempt out of component
-    const activeElement = document.activeElement;
+    // Begin logic to wrap focus if tab attempt out of component ...
+    const { activeElement } = document;
     let focusTargetElement = null;
 
-    // Forward tab with active lastElement: set focus to first focusable element
+    // 1 Forward tab on active lastElement? Set focus to first focusable element
     if (forwardDirection && activeElement === lastElement) {
         focusTargetElement = firstElement;
     }
 
-    // Backward tab with active firstElement: set focus to last focusable element
+    // 2 Backward tab on active firstElement? Set focus to last focusable element
     if (backwardDirection && activeElement === firstElement) {
         focusTargetElement = lastElement;
     }
 
+    // 3 Need a wrap? Force focus to wrap target, swallow key-down event.
     if (focusTargetElement) {
         focusTargetElement.focus();
-        return event.preventDefault();
+        event.preventDefault();
     }
 }
 
